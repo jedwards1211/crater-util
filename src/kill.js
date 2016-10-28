bb568d21a180aea0a424cfee3dd1282fd6ce5e97
Andy Edwards <jedwards@fastmail.com>
2016-10-28 00:10:24 -0500

    rewrite kill

--- src/kill.js
+++ src/kill.js
@@ -1,6 +1,30 @@
-import {customize} from 'async-child-process/lib/kill'
-import terminate from 'terminate'
+import psTree from 'ps-tree'
+import type {ChildProcess} from 'child_process'
+import promisify from 'es6-promisify'
 
-const kill = customize(child => terminate(child.pid))
-export default kill
+export default async function kill(child: ChildProcess, signal?: string = 'SIGTERM'): Promise<void> {
+  const children = await promisify(psTree)(child.pid)
+  children.forEach(child => {
+    try {
+      process.kill(parseInt(child.PID), signal)
+    } catch (err) {
+      // ignore
+    }
+  })
+  process.kill(child.pid, signal)
+
+  async function waitUntilKilled(): Promise<void> {
+    while (true) { // eslint-disable-line no-constant-condition
+      try {
+        process.kill(child.pid, 0)
+        children.forEach(child => process.kill(parseInt(child.PID), signal))
+        break
+      } catch (error) {
+        // keep looping
+      }
+    }
+  }
+
+  await waitUntilKilled()
+}
 
